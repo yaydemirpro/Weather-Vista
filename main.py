7dba9ee1dc527a9b0d9e997c7a57fa22edd84264
Yusuf A. <yaydemirpro@gmail.com>
2024-02-06 00:47:52 +0100

    city table has been updated. You can now search city weather from the city table directly. Also you can search cities by searching. You can search cities actively when typing.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QComboBox,QTableWidgetItem
-from PyQt5.QtCore import QSettings
+from PyQt5.QtWidgets import QCompleter
+from PyQt5.QtCore import QSettings,QStringListModel
 from PyQt5.QtGui import QPixmap
 from PyQt5.uic import loadUi
 from db_connect import get_db_connection, get_complete_url
@@ -39,8 +40,93 @@ class WeatherVista(QMainWindow):
         # Change cities according to country
         self.countries_list.currentIndexChanged.connect(self.fill_cities_into_combobox)
         self.cities_list.currentIndexChanged.connect(self.update_city_label)
-
         
+        #search for cities from search QLineEdit
+        self.search_city.setPlaceholderText("Search City")
+        self.searched_cities = []
+        self.selected_country = ""
+        self.selected_city = ""
+        # Create a QCompleter and set it for the search_city QLineEdit
+        self.completer = QCompleter()
+        self.search_city.setCompleter(self.completer)
+
+        # Connect the completer to a custom filtering function
+        self.completer.activated.connect(self.handle_completer_activated)
+        
+        # Connect the textChanged signal to the search_city_with_QLineEdit method
+        self.search_city.textChanged.connect(self.search_city_with_QLineEdit)
+        
+        # Connect the cellClicked signal to the handle_cell_clicked method
+        self.tableWidget.cellClicked.connect(self.handle_cell_clicked)
+        
+    def handle_cell_clicked(self, row, column):
+        # Handle the cell click event
+        if column == 0:  # Assuming the city name is in the first column
+            selected_city_name = self.tableWidget.item(row, column).text()
+            # Assuming that the city_name_label should be updated
+            self.city_name_label.setText(selected_city_name)
+
+            # Find the city data for the selected city
+            selected_city_data = next((city_data for city_data in self.cities if city_data["city"] == selected_city_name), None)
+
+            if selected_city_data:
+                # Update labels with data from the selected city
+                self.city_name_label.setText(selected_city_data["city"])
+                self.province_label.setText(selected_city_data["province"])
+                self.population_label.setText(str(selected_city_data["population"]))
+
+                # Update the cities_list combobox
+                selected_country = selected_city_data["country"]
+                self.update_country_and_city_comboboxes(selected_country, selected_city_name)
+        
+    def search_city_with_QLineEdit(self, text):
+        # Şehir araması yapılır
+        self.searched_cities = []
+
+        db_connection = get_db_connection()
+
+        for document in db_connection.find():
+            if "_id" not in document:
+                continue  # Skip documents without "_id" field
+
+            for country, cities in document.items():
+                if country == "_id":
+                    continue  # Skip the "_id" field
+
+                for city_data in cities:
+                    if text.lower() in city_data["city"].lower():
+                        self.searched_cities.append({"country": country, "city": city_data["city"]})
+
+
+        # Update the completer model with matching city names
+        completer_model = QStringListModel([city["city"] for city in self.searched_cities])
+        self.completer.setModel(completer_model)
+        # If there is a match, update the comboboxes
+        if self.searched_cities:
+            selected_country = self.searched_cities[0]["country"]
+            selected_city = self.searched_cities[0]["city"]
+            self.update_country_and_city_comboboxes(selected_country, selected_city)
+
+
+    def handle_completer_activated(self, index):
+        # Handle the completion item selection if needed
+        selected_city_name = self.completer.currentCompletion()
+
+
+    def update_country_and_city_comboboxes(self, selected_country, selected_city):
+        # Ülke ve şehir bilgilerini güncelle ve combobox'ları ayarla
+        country_index = self.countries_list.findText(selected_country)
+        if country_index != -1:
+            self.countries_list.setCurrentIndex(country_index)
+
+        self.fill_cities_into_combobox(country_index)
+
+        city_index = self.cities_list.findText(selected_city)
+        if city_index != -1:
+            self.cities_list.setCurrentIndex(city_index)
+
+            
+                    
     def fill_countries_into_combobox(self):
         db_connection = get_db_connection()    
         all_documents = db_connection.find()
