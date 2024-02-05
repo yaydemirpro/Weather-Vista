f088a7c6917865968d2ee9a5ccd9b27b2fd956e6
ismailuzum <ismailluzum@gmail.com>
2024-02-05 13:46:04 +0100

    added daily_forecast to forecastFromApi.py

--- weatherFromApi.py
+++ weatherFromApi.py
@@ -26,6 +26,10 @@ def get_weather_from_api(city_name):
         weather_info = weather_data["weather"]
         weather_description = weather_info[0]["description"]
         
+        #icon
+        weather_info = weather_data ["weather"]
+        weather_icon = weather_info[0]["icon"]
+        
         
 
         # Displaying the information
@@ -35,9 +39,10 @@ def get_weather_from_api(city_name):
         print(f"Humidity (in percentage) = {humidity}%")
         print(f"Wind Speed = {wind_speed} m/s")
         print(f"Weather description = {weather_description}")
+        print(f"Icon:{weather_icon}")
 
 
-        insert_weather_data(city_name, { "temperature": temperature, "pressure": pressure, "humidity": humidity, "wind_speed": wind_speed, "weather_description": weather_description})
+        insert_weather_data(city_name, { "temperature": temperature, "pressure": pressure, "humidity": humidity, "wind_speed": wind_speed, "weather_description": weather_description,"Icon": weather_icon})
 
     else:
         print(f"{city_name} Not Found")
@@ -59,5 +64,5 @@ def insert_weather_data(city_name, weather_data):
         print(f'PyMongo error: {pe}')
 
 # Example 
-get_weather_from_api("Amsterdam")
+get_weather_from_api("Groningen")
 
